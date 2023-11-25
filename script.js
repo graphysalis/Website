dd578dbf3f21b97c9104b3839566a8b9b0b2c0b3
Graphysalis <122803388+graphysalis@users.noreply.github.com>
2023-11-25 17:44:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const navbar = document.querySelector(".menu");
 const sideBar = document.querySelector("#side-bar");
 const content = document.querySelector(".content");
 const arrow3 = document.querySelector(".arrow3");
+const linksBox = document.querySelector(".linksBox");
 
-// au scroll > 70 => sideBar position top 0
+// au scroll > 70 => sideBar/menu position top 0
 window.addEventListener("scroll", () => {
   if (window.scrollY > 70) {
     navbar.style.top = "0px";
     sideBar.style.top = "40px";
-    arrow3.style.opacity = 1;
+    arrow3.style.display = "flex";
   } else {
     navbar.style.top = "52px";
     sideBar.style.top = "95px";
-    arrow3.style.opacity = 0;
+    arrow3.style.display = "none";
   }
 });
 
